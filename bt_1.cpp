4aa94229b3306941be412e5ecd6acb58d47282cb
Abhinav Sharma <abhimesteryo619@gmail.com>
2018-04-25 01:57:39 +0530

    Inorder function added

--- bt_1.cpp
+++ bt_1.cpp
@@ -144,6 +144,45 @@ void PreOrderTraversal(struct tNode *root)
     } /* End of if condition current->left == NULL*/
   } /* End of while */
 }
+
+{
+  /* set current to root of binary tree */
+  struct tNode *current = root;
+  struct sNode *s = NULL;  /* Initialize stack s */
+  bool done = 0;
+ 
+  while (!done)
+  {
+    /* Reach the left most tNode of the current tNode */
+    if(current !=  NULL)
+    {
+      /* place pointer to a tree node on the stack before traversing 
+        the node's left subtree */
+      push(&s, current);                                               
+      current = current->left;  
+    }
+        
+    /* backtrack from the empty subtree and visit the tNode 
+       at the top of the stack; however, if the stack is empty,
+      you are done */
+    else                                                             
+    {
+      if (!isEmpty(s))
+      {
+        current = pop(&s);
+        printf("%d ", current->data);
+ 
+        /* we have visited the node and its left subtree.
+          Now, it's right subtree's turn */
+        current = current->right;
+      }
+      else
+        done = 1; 
+    }
+  } /* end of while */ 
+}     
+
+
 int main()
 {
 int b,element;                                      
