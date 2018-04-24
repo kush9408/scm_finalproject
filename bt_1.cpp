f941f3e1f196b0152c18e93dfc9d4c37650d79f4
Abhinav Sharma <abhimesteryo619@gmail.com>
2018-04-25 01:43:47 +0530

    Preorder function added

--- bt_1.cpp
+++ bt_1.cpp
@@ -102,6 +102,48 @@ void postorder(struct Node* head)
         }
     }
 }
+
+void PreOrderTraversal(struct tNode *root)
+//Added a preorder traveral function without recursion
+{
+  struct tNode *current,*pre;
+ 
+  if(root == NULL)
+     return; 
+ 
+  current = root;
+  while(current != NULL)
+  {                 
+    if(current->left == NULL)
+    {
+      printf("%d ", current->data);
+      current = current->right;      
+    }    
+    else
+    {
+      /* Find the inorder predecessor of current */
+      pre = current->left;
+      while(pre->right != NULL && pre->right != current)
+        pre = pre->right;
+ 
+      /* Make current as right child of its inorder predecessor */
+      if(pre->right == NULL)
+      {
+        pre->right = current;
+        current = current->left;
+      }
+             
+      /* Revert the changes made in if part to restore the original 
+        tree i.e., fix the right child of predecssor */   
+      else 
+      {
+        pre->right = NULL;
+        cout<<current->data;endl;
+        current = current->right;      
+      } /* End of if condition pre->right == NULL */
+    } /* End of if condition current->left == NULL*/
+  } /* End of while */
+}
 int main()
 {
 int b,element;                                      
