818689fa309130a9c8568f91e81ce599925c1a8c
lakshika <pariharlakshika@gmail.com>
2018-04-25 23:49:49 +0530

    Level order function added by lakshika

--- bt_1.cpp
+++ bt_1.cpp
@@ -238,7 +238,33 @@ r++;
 }
 //Program to find height of binary tree
 
-
+void printLevelOrder(Node *root)
+{
+    // Base Case
+    if (root == NULL)  return;
+ 
+    // Create an empty queue for level order tarversal
+    queue<Node *> q;
+ 
+    // Enqueue Root and initialize height
+    q.push(root);
+ 
+    while (q.empty() == false)
+    {
+        // Print front of queue and remove it from queue
+        Node *node = q.front();
+        cout << node->data << " ";
+        q.pop();
+ 
+        /* Enqueue left child */
+        if (node->left != NULL)
+            q.push(node->left);
+ 
+        /*Enqueue right child */
+        if (node->right != NULL)
+            q.push(node->right);
+    }
+}//print level order by lakshika
 >>>>>>> origin12/abhinav2
 int main()
 {
